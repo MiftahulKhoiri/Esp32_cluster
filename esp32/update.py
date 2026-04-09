00996ba9dca2ed9ddf7059d7cfe4fb2008548378
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-04-10 02:07:57 +0800

    Create update.py

---
+++ esp32/update.py
@@ -0,0 +1,29 @@
+import network
+import time
+from config import WIFI_SSID, WIFI_PASSWORD
+import ota
+
+
+
+def connect_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+
+    if not wlan.isconnected():
+        print("Connecting WiFi...")
+        wlan.connect(WIFI_SSID, WIFI_PASSWORD)
+
+        timeout = 0
+        while not wlan.isconnected():
+            time.sleep(1)
+            timeout += 1
+
+            if timeout > 20:
+                print("WiFi failed")
+                return
+
+    print("WiFi connected")
+    print(wlan.ifconfig())
+
+connect_wifi()
+ota.perform_update()
