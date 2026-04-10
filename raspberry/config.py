10e8d0ad072ec8a392d40a395093e5e8b5bd9284
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-04-10 11:45:46 +0800

    Update config.py

--- raspberry/config.py
+++ raspberry/config.py
@@ -2,14 +2,13 @@ MQTT_BROKER = "localhost"
 
 TASK_DISPATCH_INTERVAL = 1
 
-TASK_TIMEOUT = 30
-
 RETRY_LIMIT = 3
 
+TASK_TIMEOUT = 30
+
+NODE_HEARTBEAT_TIMEOUT = 60
+
 DEFAULT_TASK = {
-
     "task": "random",
-
     "count": 10
-
 }
